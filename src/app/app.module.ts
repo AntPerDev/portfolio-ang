5e63968ed8fa825d51692150130340b32caa287d
Antonio Pérez <antperdev@gmail.com>
2022-07-04 13:46:04 +0200

    Added styles, interfaces, infoService, data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 //Rutas
 import { AppRoutingModule } from './app-routing.module';
 
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './share/header/header.component';
-import { FooterComponent } from './share/footer/footer.component';
-import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './share/footer/footer.component';
+import { HeaderComponent } from './share/header/header.component';
 import { ItemComponent } from './pages/item/item.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    PortafolioComponent,
     AboutComponent,
-    ItemComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    ItemComponent,
+    PortafolioComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
