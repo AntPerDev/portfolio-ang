9fd34a134ad0662694525864b99867fb674417e0
Antonio Pérez <antperdev@gmail.com>
2022-07-07 09:57:38 +0200

    added circle ci

--- src/app/services/info-pagina.service.spec.ts
+++ src/app/services/info-pagina.service.spec.ts
@@ -1,16 +1,1 @@
-import { TestBed } from '@angular/core/testing';
 
-import { InfoPaginaService } from './info-pagina.service';
-
-describe('InfoPaginaService', () => {
-  let service: InfoPaginaService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(InfoPaginaService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
